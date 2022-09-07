b6b976822917b7074da7db4c28c7729a2a79d75a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-07 20:32:45 -0300

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,8 @@
 #!/usr/bin/pyton3
-
 def uppercase(str):
+    for i in str:
+        charNum = ord(i)
+        if (charNum >= 97 and charNum <= 122):
+            charNum = charNum - 32
+        print("{}".format(chr(charNum)), end="")
+    print()
