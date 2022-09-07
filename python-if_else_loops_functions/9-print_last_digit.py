c0646b14bf2cf2346803a3d6cc6a97f20941dd22
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-07 20:44:35 -0300

    task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -7,5 +7,4 @@ def print_last_digit(number):
         numberStr = repr(number)
         print("{}".format(numberStr[-1]), end="")
         return (numberStr[-1])
-    else
-        return NULL
+    return
