57d7e44c5d2664ec3e7d13ee518ecfb03ae74d39
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-10 21:18:36 -0300

    fixed pep8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
 
     argCount = len(argv) - 1
 
+# PRINTING FIRST LINE
     if (argCount == 0):
         print("{} arguments.".format(argCount))
     elif (argCount == 0 or argCount != 1):
@@ -11,5 +12,6 @@ if __name__ == "__main__":
     else:
         print("{} argument:".format(argCount))
 
+# PRINTING ARGUMENTS
     for i in range(1, argCount + 1):
         print("{}: {}".format(i, argv[i]))
