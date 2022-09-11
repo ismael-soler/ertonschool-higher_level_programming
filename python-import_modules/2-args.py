a97337c0bb358a3ceb735c65eecf9e23203152bd
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-10 21:16:29 -0300

    task 2 v2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-
-from ast import arg
-
-
 if __name__ == "__main__":
     from sys import argv
-
 
     argCount = len(argv) - 1
 
     if (argCount == 0):
         print("{} arguments.".format(argCount))
-    else:
+    elif (argCount == 0 or argCount != 1):
         print("{} arguments:".format(argCount))
+    else:
+        print("{} argument:".format(argCount))
 
     for i in range(1, argCount + 1):
         print("{}: {}".format(i, argv[i]))
