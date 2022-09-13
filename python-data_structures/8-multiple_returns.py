d253b745ce85887b64afd7ea1f25e1235a7b0186
Ismael Soler <ismaelsolerferreira@gmail.com>
2022-09-13 11:08:50 -0300

    commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (sentence == None or sentence == ""):
+    if (sentence == ""):
         return (0, "None")
     firstChar = sentence[:1]
     sentenceLength = len(sentence)
