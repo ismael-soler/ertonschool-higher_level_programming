00bd3a4cef9352591eb5d687c3615884c6052e8a
Ismael Soler <ismaelsolerferreira@gmail.com>
2022-09-13 09:28:00 -0300

    task 6 v4

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
+    if matrix == None:
+        return
     for row in matrix:
         for j in range(0, len(row)):
             if j == len(row) - 1:
