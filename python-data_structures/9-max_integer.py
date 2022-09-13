4581af05987bb144b3c1b91f6d4395b32515d9cc
Ismael Soler <ismaelsolerferreira@gmail.com>
2022-09-13 11:17:19 -0300

    commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None or len(my_list) == 0:
         return None
     currentBigger = 0
     for i in my_list:
