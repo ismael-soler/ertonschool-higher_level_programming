fe353834b4cef496f15ec4db48c07c47e08630c1
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-15 22:47:29 -0300

    task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from unittest.mock import NonCallableMagicMock
+
+
+def multiply_list_map(my_list=[], number=0):
+    if my_list is None:
+        return
+
+    return list(map(lambda x: x * number, my_list.copy()))
