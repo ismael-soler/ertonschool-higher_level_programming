8c24e7eda47bf52ca4315f1b3f6f347b5d7b519f
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 11:32:10 -0300

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    if a is None or b is None:
+        return
+
+    toReturn = 0
+    try:
+        divResult = a/b
+        toReturn = divResult
+
+    except ZeroDivisionError:
+        toReturn = None
+
+    finally:
+        print("Inside result: {}".format(toReturn))
+
+    return toReturn
