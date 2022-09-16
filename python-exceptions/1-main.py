f5b19c3811288302ad7570271cbe646c4c408087
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 13:13:28 -0300

    removed main files

--- python-exceptions/1-main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
-
-value = None
-print(safe_print_integer(value))
--- python-more_data_structures/12-main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" Test file
-"""
-roman_to_int = __import__('12-roman_to_int').roman_to_int
-
-print(roman_to_int(None))
