cf351166636c9f1f9b8a7d6c0772cb344cc2db48
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 12:57:01 -0300

    task 4 v2

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from decimal import DivisionByZero
-
-
 def list_division(my_list_1, my_list_2, list_length):
     if my_list_1 is None or my_list_2 is None or list_length is None:
         return
