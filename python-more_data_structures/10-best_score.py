24a09f96286cfc3b50f52f2bf7c6d5f43efd7001
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-15 22:42:39 -0300

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+from curses import keyname
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    biggestValue = 0
+    biggestKey = ""
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > biggestValue:
+            biggestValue = a_dictionary[key]
+            biggestKey = key
+
+    return biggestKey
