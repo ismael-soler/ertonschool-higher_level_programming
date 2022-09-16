500ff15d53a51d5824bdccb43d94f3c6e37c13ed
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-15 22:03:47 -0300

    task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from hashlib import new
+
+
+def only_diff_elements(set_1, set_2):
+    if set_1 is None or set_2  is None:
+        return
+
+    newSet = set_1.symmetric_difference(set_2)
+    return newSet
