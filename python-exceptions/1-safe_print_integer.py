b6f4bfa808a21ca421529f337a2369f4265317fd
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 11:00:43 -0300

    task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    if value is None:
+        return
+    condition = False
+
+    try:
+        print("{:d}".format(value))
+        condition = True
+
+    except:
+        condition = False
+
+
+    return condition
