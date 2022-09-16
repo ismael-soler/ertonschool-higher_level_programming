22dc14fd2d983ce5737cf24f579fccc461989f25
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 11:13:39 -0300

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if my_list is None:
+        return
+
+    counter = 0
+    try:
+        for i in range(0, x):
+            intCondition = isinstance(my_list[i], int)
+            if intCondition is True:
+                print("{:d}".format(my_list[i]), end="")
+                counter += 1
+
+    except IndexError:
+        raise IndexError
+    else:
+        print()
+        return counter
