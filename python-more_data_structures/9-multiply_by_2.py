1de917c884ee37f0ab78a02bbde7f56213d9c365
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-15 22:36:48 -0300

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary is None:
+        return a_dictionary
+
+    newDictionary = a_dictionary.copy()
+    for key in newDictionary.keys():
+        newDictionary[key] = newDictionary[key]*2
+
+    return newDictionary
