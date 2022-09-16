615513d0a3cf710af46ca84c58e681066b922393
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 12:56:25 -0300

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+from decimal import DivisionByZero
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    if my_list_1 is None or my_list_2 is None or list_length is None:
+        return
+
+    result = []
+
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i]/my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            continue
+
+    return result
