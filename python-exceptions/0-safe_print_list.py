910bcef397ee2696380d566be6bc5eab79d4829a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-16 10:51:50 -0300

    task 12

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list is None:
+        return
+
+    counter = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            counter += 1
+
+    except IndexError:
+        print()
+    else:
+        print()
+
+    return counter
