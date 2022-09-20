11eec23287d945c19a78ee482aa66fdc4995c8bf
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-20 09:52:57 -0300

    task 2 v0

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """Init class"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integrer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
