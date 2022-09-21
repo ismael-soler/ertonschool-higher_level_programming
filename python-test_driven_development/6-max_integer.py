2cc2a7801a2b071c8192cf18312b6db6bf350ee9
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-21 16:47:15 -0300

    commit

--- python-test_driven_development/6-max_integer.py
+++ python-test_driven_development/6-max_integer.py
@@ -14,15 +14,4 @@ def max_integer(list=[]):
         if list[i] > result:
             result = list[i]
         i += 1
-
-    # If first element => find another integer
-    if list[0] == result and len(list) > 1:
-        result = list[0]
-        i = 1
-        while i < len(list):
-            if list[i] != list[0]:
-                result = list[i]
-                break
-            i += 1
-
     return result
