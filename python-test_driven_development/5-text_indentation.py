95fed4b6ba44aa4b85ee0bb366b5b831e5a4dde9
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-21 16:04:42 -0300

    commit

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" indents text """
+
+
+def text_indentation(text):
+    """ indentator """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    punctuationWasFound = False
+    for i in range(len(text)):
+        if punctuationWasFound:
+            if text[i] == " ":
+                continue
+            punctuationWasFound = False
+            continue
+        print(text[i], end="")
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            punctuationWasFound = True
+            print()
+            print()
