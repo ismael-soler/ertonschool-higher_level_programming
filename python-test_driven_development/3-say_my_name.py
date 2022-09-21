c0c0d0ad8a528172355107bf447a079952f966a7
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-21 15:25:33 -0300

    task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" sends a message with a name """
+
+
 def say_my_name(first_name, last_name=""):
+    """ prints message """
+
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
