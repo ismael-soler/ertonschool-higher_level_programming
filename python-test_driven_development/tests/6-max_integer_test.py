a0d27a0aa6710c113890a5ae78567aa081a8257a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-21 16:38:25 -0300

    commit

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,10 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Unittest class for tests"""
+
+    def testForList(self):
+        """Test a list"""
+        result = max_integer([1, 2, 3, 4])
+        self.assertEqual(result, 4)
+
