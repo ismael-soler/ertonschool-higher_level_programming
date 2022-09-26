de73db989aeb92741859c817f457fc02b7e974ad
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-26 12:32:30 -0300

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" returns some stuff"""
+
+
+def is_same_class(obj, a_class):
+    """checks if it's the same class"""
+    if a_class == type(obj):
+        return True
+    else:
+        return False
