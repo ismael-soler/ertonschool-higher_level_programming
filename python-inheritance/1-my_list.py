07eb68a96dd679a95d2baffe96ad18c51317e526
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-26 11:42:57 -0300

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" write a class that inherits from list"""
+
+
+class MyList(list):
+    """ MyList """
+
+    def __init__(self):
+        pass
+
+    def print_sorted(self):
+        lst = self.copy()
+        lst.sort()
+        print(lst)
