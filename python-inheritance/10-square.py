7ca2172435a048aa6a1903315bc38c5bcf0899ea
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-26 16:01:47 -0300

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" New class based on rectangle """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square child of Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ new area method, returns the area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ prints the dimensions """
+        return super().__str__()
