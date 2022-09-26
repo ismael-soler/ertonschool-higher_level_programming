527d48e8e737f41f032ddbf487d055d1a78648a7
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-26 14:23:35 -0300

    task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Write an empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """ Base Geometry """
+
+    def area(self):
+        raise Exception("are() is no implemented")
