d41bc2d94e8e991eef7cb44d60d2b02c344b6f0a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-26 13:44:44 -0300

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""" returns if the object is an instance of a  class """
+
+
+def inherits_from(obj, a_class):
+    """ checks the condition"""
+    return isinstance(type(obj), a_class) or type(obj) is a_class
