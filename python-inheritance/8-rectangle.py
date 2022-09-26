aee4a71bb66afbfd9cd80192227c8f6692dbdd53
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-26 15:46:34 -0300

    task 9

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write an empty class BaseGeometry"""
+""" Write a new class BaseGeometry"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Write a new class BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle child of BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        return self.__width * self.__height
