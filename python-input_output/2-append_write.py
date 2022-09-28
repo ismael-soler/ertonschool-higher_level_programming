8bb45b4ac336944034f10dbe7c9d9d98083f3a4f
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-28 11:11:19 -0300

    task 1

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" appends a string to a text file (UTF8)  """
+
+
+def write_file(filename="", text=""):
+    """ writes to the file """
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
