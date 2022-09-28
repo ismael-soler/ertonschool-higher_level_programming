48fcbaae141a78fe3419f7820cc5ada688e0bbab
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-28 15:22:16 -0300

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,14 +4,19 @@
 
 def pascal_triangle(n):
     """ creates the triangle """
-    if n <= 0 :
+    if n <= 0:
         return []
 
-    listOfLists = []
-    for i in range(n):
-        rowList = []
-        row = pow(11, i)
-        for number in str(row):
-            rowList.append(int(number))
-        listOfLists.append(rowList)
-    return listOfLists
+    triangle = []
+    triangle.append([1])
+    firstLoop = True
+    for i in range(1, n):
+        triangle.append([1])
+        for j in range(1, i + 1):
+            try:
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            except IndexError:
+                triangle[i].append(1)
+                continue
+
+    return triangle
