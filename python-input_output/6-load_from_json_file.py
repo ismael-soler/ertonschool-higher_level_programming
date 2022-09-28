d4d18066e3ae61c38ad95ff264422a9f68bf0f48
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-28 11:40:57 -0300

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" creates an object from a "JSON file" """
+import json
+
+
+def load_from_json_file(filename):
+    """ main """
+    with open(filename, 'r', encoding='utf-8') as new_obj:
+        return json.load(new_obj)
