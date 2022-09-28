de6a66f939300b49bcf547bdf83311a9d69847a0
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-28 13:22:06 -0300

    commit

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 """ Write a class Student """
-
-
-from csv import DictReader
-from operator import attrgetter
-
-
 class Student:
     """ Class Student """
     def __init__(self, first_name, last_name, age):
