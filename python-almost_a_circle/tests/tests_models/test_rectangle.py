81563a5229676d2760c011ca5285acc6c7fbd80d
ismael-soler <ismaelsolerferreira@gmail.com>
2022-10-05 14:02:18 -0300

    udpate2

--- python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
 import unittest
-from models.base import Base
 from models.rectangle import Rectangle
 
 
 class RectangleTest(unittest.TestCase):
 
     def test_Rectangle(self):
+        """
+        Test that the id, width, height, x, and y attributes are set correctly.
+        """
         r1 = Rectangle(1, 2)
         self.assertEqual(r1.id, 1)
         self.assertEqual(r1.width, 1)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
