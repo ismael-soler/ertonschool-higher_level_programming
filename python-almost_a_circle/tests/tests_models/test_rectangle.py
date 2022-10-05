5b86c49aac9de1e89ec9d9f66a2fdb016f9d6b42
ismael-soler <ismaelsolerferreira@gmail.com>
2022-10-05 13:43:22 -0300

    fix

--- python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -3,9 +3,14 @@
 import unittest
 from models.rectangle import Rectangle
 
+
 class RectangleTest(unittest.TestCase):
 
     def test_Rectangle(self):
         self.assertEqual(type(Rectangle(1, 2)), Rectangle)
         self.assertEqual(type(Rectangle(1, 2, 3)), Rectangle)
         self.assertEqual(type(Rectangle(1, 2, 3, 4)), Rectangle)
+
+
+if __name__ == '__main__':
+    unittest.main()
