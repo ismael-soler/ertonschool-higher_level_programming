bcd4904410f11b925612acbab8ab9fb67135dc33
ismael-soler <ismaelsolerferreira@gmail.com>
2022-10-05 14:00:56 -0300

    udpate

--- python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -7,12 +7,13 @@ from models.rectangle import Rectangle
 
 class RectangleTest(unittest.TestCase):
 
-    def test_rectangle_subclass(self):
-        """
-        Test if Rectangle class inherit from
-        Base class
-        """
-        self.assertTrue(issubclass(Rectangle, Base))
+    def test_Rectangle(self):
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
 
 if __name__ == '__main__':
     unittest.main()
