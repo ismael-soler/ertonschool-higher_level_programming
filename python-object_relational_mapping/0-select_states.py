59bd9c1789abc337e3cf3919c5d49fd63474403a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-11-08 19:22:33 -0300

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_dbname,
+        port=3306
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * from states")
+    stuff = cur.fetchall()
+    for i in stuff:
+        print(i)
+
+    cur.close()
+    db.close()
