1ce37196c691e28c957200ab6352509df3bd9641
ismael-soler <ismaelsolerferreira@gmail.com>
2022-11-13 18:18:25 -0300

    commit

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        mysql_username, mysql_password, mysql_dbname), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name.like('%a%'))
+    for item in result:
+        session.delete(item)
+
+    session.commit()
+    session.close()
