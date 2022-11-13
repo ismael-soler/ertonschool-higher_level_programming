3a043e5dc65e304b986f7320c6323b17097fe9d7
ismael-soler <ismaelsolerferreira@gmail.com>
2022-11-13 18:12:03 -0300

    11

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""MODULE NAME"""
+"""script that prints the State object with
+the name passed as argument from the database
+hbtn_0e_6_usa"""
 
 
 from sqlalchemy import create_engine
