5d4ab6174b13f54cbd8702a0af422b1fd85d7290
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-09 19:34:28 -0300

    commit

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 const process = require('process');
 const args = process.argv;
 
-if (args[3]) {
-  console.log(args[3]);
+if (args[2]) {
+  console.log(args[2]);
 } else {
   console.log('No argument');
 }
