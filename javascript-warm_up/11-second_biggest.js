179ada743e006eb3ad7fe874a67c926dfb4fe6f3
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-09 20:35:10 -0300

    commit

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,17 @@
 const process = require('process');
 const args = process.argv;
 
-const newList = [];
-// removes unused arguments
-args.splice(0, 2);
-// converts every item into an int
-args.forEach(element => {
-  newList.push(parseInt(element));
-});
-// sorts
-newList.sort((a, b) => b - a);
-console.log(newList[1]);
+if (!args || args.length === 3) {
+  console.log('0');
+} else {
+  const newList = [];
+  // removes unused arguments
+  args.splice(0, 2);
+  // converts every item into an int
+  args.forEach(element => {
+    newList.push(parseInt(element));
+  });
+  // sorts
+  newList.sort((a, b) => b - a);
+  console.log(newList[1]);
+}
