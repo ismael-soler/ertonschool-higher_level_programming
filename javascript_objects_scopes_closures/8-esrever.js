d13b5c610d6dac219269975f94860583b4cd0490
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-11 17:16:00 -0300

    commit

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let newList = []
+  let newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    newList.push(list[i])
+    newList.push(list[i]);
   }
-  return (newList)
+  return (newList);
 };
