ea0f364e4784d04d4bac2ec20eb3f4b7d19593c5
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-11 16:31:10 -0300

    task 4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -16,5 +16,14 @@ class Rectangle {
       console.log(row);
     }
   }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 module.exports = Rectangle;
