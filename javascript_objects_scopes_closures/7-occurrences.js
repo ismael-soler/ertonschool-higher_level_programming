989ffe1a4f98436423c53bba05b474c80f43c57a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-11 17:05:22 -0300

    commit

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let counter = 0;
-  list.forEach(element => {
-    if (element === searchElement) {
-      counter += 1;
-    }
-  });
-  console.log(counter)
+  return (list.filter(element => element === searchElement).length);
 };
