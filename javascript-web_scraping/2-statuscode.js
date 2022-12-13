2fbfbe4aa1c9aeb356db5ff2a1288c3d87eb8c86
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-13 19:04:33 -0300

    task 2: '

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const urlToCheck = process.argv[2];
+
+request(urlToCheck, function (error, response) {
+  if (error) {
+    console.log('An error occurred: ' + error.message);
+  } else {
+    console.log('code ' + response.statusCode);
+  }
+});
