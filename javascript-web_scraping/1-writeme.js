189e7600f3225545ace3be58a8b76df3044be963
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-13 18:48:16 -0300

    commit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-const fileContent = fs.writeFileSync(filePath, string, { encoding: 'utf8' });
+fs.writeFileSync(filePath, string, { encoding: 'utf8' });
