53a41a8960abf60f463786368709d5b9cf14a305
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-13 19:56:32 -0300

    tasl 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,16 +6,13 @@ request(apiURL, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     // Converts the JSON from a string to an object
     const data = JSON.parse(body);
-    let counter = 0;
 
-    // Check
-    data.results.forEach(movie => {
-      if (movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        counter++;
-      }
+    const films = data.results.filter((film) => {
+      return film.characters.includes('https://swapi-api.hbtn.io/api/people/18/');
     });
-    console.log(counter);
+
+    console.log(films.length);
   } else {
-    console.log('Error: could not retrieve movie information');
+    console.log('Error: could not retrieve character information');
   }
 });
